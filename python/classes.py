db8ac1e414a2c6794f545213ff7cfd8e03c2a8de
Titobiloluwa Olatunji <olatunjititobi@gmail.com>
2023-08-31 03:05:34 +0200

    update

---
+++ python/classes.py
@@ -0,0 +1,9 @@
+# A class is a template for a type of object
+class point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+
+p = point(2, 8)
+print(p.x)
+print(p.y)
