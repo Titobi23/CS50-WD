c995d14099d791c656856df9091299fd8bb6804c
Titobiloluwa Olatunji <olatunjititobi@gmail.com>
2023-08-31 02:25:51 +0200

    update

---
+++ python/lists.py
@@ -0,0 +1,12 @@
+# Define a list of names
+names = ["Potter", "Ron", "Hermione", "Ginny", "voldemort"]
+
+print(names)
+
+# Recall, lists are mutable
+names.append("Draco")
+print(names)
+
+# sorting
+names.sort()
+print(names)
