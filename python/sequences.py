60403571b145e7b8b70e4adfa43a429205533164
Titobiloluwa Olatunji <olatunjititobi@gmail.com>
2023-08-31 02:19:11 +0200

    update

--- python/sequences.py
+++ python/sequences.py
@@ -1,4 +1,20 @@
 # Sequences
 name = "Barry"
 
-print(name[0])
+print(name[1])
+
+# lists
+names = ["Potter", "Ron", "Hermione"]
+print(names)
+print(names[0])
+
+# Tuples
+# Doesn't change
+coordinate = (10.0, 20.0)
+print("x: {}, y:{}".format(*coordinate))
+
+# Data Structures
+# 1. list - sequence of mutable values
+# 2. tuple - sequence of immutable sequences
+# 3. set - onordered collection of unique values. Values show up only once
+# 4. dictionary(dict) - collection of key-value pairs
